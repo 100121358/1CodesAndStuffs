d19fb86a7354596eebd2cf9ee4692be63ac16add
100121358 <100121358@lubbockisd.net>
2019-10-24 08:47:29 -0500

    concatenation

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -20,5 +20,21 @@ print(fullName)
 print("Hip"*2 + "Hooray!")
 def rowYourBoat():
     print("Row, "*3 + 'your boat')
-    print()
+    print("Gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+    print("dream, "*5)
 
+rowYourBoat()
+
+# Indexing
+
+name = "Roy G Biv"
+firstChar = name[0]
+print(firstChar)
+middleIndex = len(name) // 2
+print(middleIndex)
+print(name[middleIndex])
+
+print(name[-1])
+print(name[-2])
