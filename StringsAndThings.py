ea97b8c6d98239b3476a84dce7690d2a07dffad9
100121358 <100121358@lubbockisd.net>
2019-10-24 08:34:58 -0500

    concatenation

---
+++ StringsAndThings.py
@@ -0,0 +1,24 @@
+# Strings
+#    data that falls within " " marks
+
+# concatenation
+# put 2 or more strings together
+
+firstName = "Fred"
+lastName = "Flintstone"
+
+print(firstName + " " + lastName)
+
+fullName = firstName + " " + lastName
+
+
+print(fullName)
+
+# repitition
+#  Repitition operator: *
+
+print("Hip"*2 + "Hooray!")
+def rowYourBoat():
+    print("Row, "*3 + 'your boat')
+    print()
+
